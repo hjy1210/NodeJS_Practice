c4786b4003986bf94c4c71570b12807867667fa7
hjy1210 <hjyanghj@gmail.com>
2017-02-24 00:06:44 +0800

    完成part 1的test

--- lessons/unittest/tests/part1/cart-summary-test.js
+++ lessons/unittest/tests/part1/cart-summary-test.js
@@ -4,31 +4,30 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 var CartSummary = require('./../../src/part1/cart-summary');
 
 describe('CartSummary', function() {
-  it('getSubtotal() should return 0 if no items are passed in', function() {
+  it('getSubtotal() should return 0 if no items are passed in',
+  function() {
     var cartSummary = new CartSummary([]);
     expect(cartSummary.getSubtotal()).to.equal(0);
   });
-  it('getSubtotal() should return the sum of the price * quantity for all items', function() {
-  var cartSummary = new CartSummary([{
-    id: 1,
-    quantity: 4,
-    price: 50
-  }, {
-    id: 2,
-    quantity: 2,
-    price: 30
-  }, {
-    id: 3,
-    quantity: 1,
-    price: 40
-  }]);
-
-  expect(cartSummary.getSubtotal()).to.equal(300);
+  it('getSubtotal() should return the sum of the price * quantity for all items',
+  function() {
+    var cartSummary = new CartSummary([{
+      id: 1,
+      quantity: 4,
+      price: 50
+    }, {
+      id: 2,
+      quantity: 2,
+      price: 30
+    }, {
+      id: 3,
+      quantity: 1,
+      price: 40
+    }]);
+    expect(cartSummary.getSubtotal()).to.equal(300);
+  });
 });
-});
 
-// tests/part1/cart-summary-test.js
-// ...
 var sinon = require('sinon');
 var tax = require('./../../src/part1/tax');
 
@@ -48,24 +47,31 @@ describe('getTax()', function() {
   });
 
   it('getTax() should execute the callback function with the tax amount',
-  function(done1) {
-    var cartSummary = new CartSummary([{
+  function(done) {  // done is provided by mocha for async testing.
+    // The test will timeout and error if done is not invoked
+    // within 2000 milliseconds.
+    var cartSummary = new CartSummary([
+    {
       id: 1,
       quantity: 4,
       price: 50
-    }, {
+    },
+    {
       id: 2,
       quantity: 2,
       price: 30
-    }, {
+    },
+    {
       id: 3,
       quantity: 1,
       price: 40
-    }]);
-
+    }
+    ]);
     cartSummary.getTax('NY', function(taxAmount) {
       expect(taxAmount).to.equal(30);
-      done1();
+      expect(tax.calculate.getCall(0).args[0]).to.equal(300);
+      expect(tax.calculate.getCall(0).args[1]).to.equal('NY');
+      done();
     });
   });
 });
