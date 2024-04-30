cb5102646e55507ccdf4eff0b5965f0749204f0c
hjyanghj <hjyanghj@gmail.com>
2024-04-30 11:45:00 +0800

    delete FSWD,

--- FSWD/ServerSide/rect-1.js
+++
@@ -1,8 +0,0 @@
-module.exports =  {
-    perimeter:  function(x,y) {
-      return  (2*(x+y));
-    },
-    area:   function(x,y) {
-      return  (x*y);
-    }
-}
--- FSWD/ServerSide/rect.js
+++
@@ -1,3 +0,0 @@
-var rectangle=require('./rect-1');
-console.log(rectangle.area(3,8));
-console.log(rectangle.perimeter(3,8));
